18486964403d6f884d7d17df3f0de90c577c5856
Viljoen13 <cilliers40@gmail.com>
2025-01-18 16:08:23 +0200

    "new changes"

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -18,8 +18,29 @@ func _ready():
 	
 	
 	# Fill the TileMap
-	for y in range(rows):
-		for x in range(cols):
-			set_cell(x, y, 0)  # Replace 0 with the correct tile ID
+	#for y in range(rows):
+	#	for x in range(cols):
+	#		set_cell(x, y, 0)  # Replace 0 with the correct tile ID
 	
 	
+	
+func place_tower(global_position: Vector2):
+	var snapped_position = snap_to_grid(global_position)
+	var tower_instance = preload("res://Scenes//ArcherTower.tscn").instance()
+	add_child(tower_instance)
+	tower_instance.position = snapped_position
+
+
+
+func snap_to_grid(global_position: Vector2):
+	# Convert the global position to the nearest cell
+	var cell_position = self.world_to_map(global_position)
+	# Convert the cell back to world position (center of the cell)
+	return self.map_to_world(cell_position)
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		# Get the mouse's global position
+		var mouse_position = get_global_mouse_position()
+		# Call the place_tower function with the mouse position
+		place_tower(mouse_position)
